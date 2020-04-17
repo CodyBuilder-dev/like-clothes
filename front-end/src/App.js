59bb35a335a3fa1a2bda510346f4f5cf19be7831
An yulim <db9292@naver.com>
2020-04-17 09:37:09 +0900

    [Feat] Add ClothesResister Page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,11 +3,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 // import logo from './logo.svg';
 import './App.css';
 import MainPage from './pages/MainPage';
+import ClothesResister from './pages/ClothesResisterPage';
 
 function App() {
   return (
     <Router>
       <Route path="/" exact render={() => <MainPage/>} />
+      <Route path="/clothesresister" exact component={ClothesResister} />
+      
     </Router>
 
     // <div className="App">
