7e52e038061e25cdd499ada45eaf7932bb6cda14
장현진 <jhjhyjin@naver.com>
2020-04-20 23:55:52 +0900

    [Docs] Add redux 환경설정S02P23A401-51

--- front-end/src/store/configureStore.js
+++ front-end/src/store/configureStore.js
@@ -1,1 +1,10 @@
 // reducer들을 통합할 configureStore 파일을 정의한다.
+import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducers from './reducers';
+
+export default initStates => createStore(
+  combineReducers(reducers),
+  initStates,
+  composeWithDevTools(), // 미들웨어 함수가 이런 것으로 대체되었다.
+);
