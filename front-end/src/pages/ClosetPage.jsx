f193a2060796c0c1a04aca074c3fd36924cbbb06
안유림 <db9292@naver.com>
2020-04-21 19:04:38 +0900

    [Feat] Add Closet Intro Edit S02P23A401-49

--- front-end/src/pages/ClosetPage.jsx
+++ front-end/src/pages/ClosetPage.jsx
@@ -3,51 +3,107 @@ import { Redirect, Link } from 'react-router-dom';
 import { Dropdown } from 'semantic-ui-react';
 import axios from 'axios';
 
+// semantic-ui-react Dropdown Css
 import '../css/ClosetPage.css';
 const styleLink = document.createElement("link");
 styleLink.rel = "stylesheet";
 styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
 document.head.appendChild(styleLink);
 
+
 function Closet() {
+  // 유저의 팔로 리스트 서버에서 받아오기
   const [ routeTarget, setRouteTarget ] = useState('closet');
   const [ followerRes, setFollowerRes ] = useState([]);
+  const [ followingRes, setFollowingRes ] = useState([]);
   
   useEffect(() => {
-    let url = `http://i02a401.p.ssafy.io:8000/user/follower-user`;
-    axios.get(url, {'headers': {'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IjEyM0BnbWFpbC5jb20iLCJpYXQiOjE1ODczOTc0MTksImV4cCI6MTU4NzQwNDYxOX0.W_5Ljqjd-wNKTdqaD_erKv57LXxZB2Aa2BkNecHfBmM'}})
+    let follow = ['follower-user', 'following-user', 'follower_edmail', 'following_email']
+    for (let i=0; i<2; i++){
+      let url = `http://i02a401.p.ssafy.io:8000/user/${follow[i]}`;
+      axios.get(url, {'headers': {'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IjEyM0BnbWFpbC5jb20iLCJpYXQiOjE1ODc0NTQ3NzMsImV4cCI6MTU4NzQ2MTk3M30.uqORKirVsgHeosmtdGJoCwScgxiPjXeB6ceZAjfXK8g'}})
       .then((res) => {
-        const followerList = res.data;
-        followerList.forEach((follower) => {
-          const followerKey = follower.follower_email;
-          const followerImage = follower.profile_img;
-          const followerNickname = follower.nickname;
-          let data = { key: followerKey, value: followerKey, flag: followerImage, text: followerNickname }
-          setFollowerRes(followerRes => [...followerRes, data])
+        const followList = res.data;
+        // const followList = [{ key: 1, value: 2, flag: 3, text: 4 }, { key: 1, value: 2, flag: 3, text: 4 }];
+        console.log(followList)
+        followList.forEach((follow) => {
+          const followKey = follow.follow[i+2]
+          const followImage = follow.profile_img;
+          const followNickname = follow.nickname;
+          let data = { key: followKey, value: followKey, flag: followImage, text: followNickname }
+          if (i === 0) {
+            setFollowerRes(followerRes => [...followerRes, data])
+          }
+          else {
+            setFollowingRes(followingRes => [...followingRes, data])
+          }
         });
       });
+    }
   }, []);
+  console.log(followerRes, followingRes, 'state?')
 
-  const Options = followerRes
-
+  // 드롭다운 팔로 선택 시 선택된 유저 옷장으로 이동
   const DropdownOnClick = (e) => {
     let target = e.target.innerText;
     setRouteTarget(target);
   }
 
-  const ProfileNameEdit = (e) => {
-    
+  // 옷장 소개 수정 - 내 옷장인지 확인 필요
+  const [ closetIntro, setClosetIntro ] = useState('유림스옷장');
+  const [ isEdit, setIsEdit ] = useState(true);
+
+  const handleIntroClick = (e) => {
+    setIsEdit(false)
   }
+
+  const ClosetIntroView = () => ( // true  
+    <div>
+      <span>{closetIntro}</span>
+      <button onClick={handleIntroClick}>수정버튼</button>
+    </div>
+  );
+
+  const ClosetIntroEdit = () => {  // false
+    const [ editIntro, setEditIntro ] = useState(closetIntro);
+
+    const handleIntroChange = (e) => {
+      setEditIntro(e.target.value)
+    }
+
+    const handleIntroBlur = (e) => {
+      setClosetIntro(e.target.value)
+      setIsEdit(true)
+    }
+  
+    const handleIntroEnter = (e) => {
+      if (e.key === 'Enter') {
+      setClosetIntro(e.target.value)
+      setIsEdit(true)
+      }
+    }
+
+    return (
+      <div>
+        <input type="text" value={editIntro} autoFocus
+          onChange={handleIntroChange} onBlur={handleIntroBlur} onKeyPress={handleIntroEnter}></input>
+      </div>
+    )
+  }
+  
+  // 팔로우 버튼 - 내 옷장 아닌지 확인 필요
+  const handleFollowClick = (e) => {
+
+  }
+  
 
   return (
     <div className="myCloset">
       <div className="myInfo">
         <div className="myProfile">
           <span className="profileName">Yulim</span>
-          <input type="text"></input>
-          <button onClick={ProfileNameEdit}>수정버튼</button>
-          <span>환영한다</span>
-          <button>수정버튼</button>
+          <button onClick={handleFollowClick}> follow </button>
+          {isEdit ? <ClosetIntroView /> : <ClosetIntroEdit />}
         </div>
         <div className="follow">
           <span className="followTag">Follower</span>
@@ -70,7 +126,14 @@ function Closet() {
         scrolling={true}
         search
         selection
-        options={Options}
+        options={followerRes}
+        onChange={DropdownOnClick}
+      ></Dropdown>
+      <Dropdown
+        scrolling={true}
+        search
+        selection
+        options={followingRes}
         onChange={DropdownOnClick}
       ></Dropdown>
 
