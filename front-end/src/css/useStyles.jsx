6581713bf3f84d5206d5ddbe9fa50b421fd9c72f
장현진 <jhjhyjin@naver.com>
2020-04-25 15:22:20 +0900

    [Style] Add choicestylejsx

--- front-end/src/css/useStyles.jsx
+++ front-end/src/css/useStyles.jsx
@@ -96,3 +96,14 @@ export const clothesdetailjsx = makeStyles((theme) => ({
     backgroundColor: deepPurple[500],
   },
 }));
+
+export const choicestylejsx = theme => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+});
