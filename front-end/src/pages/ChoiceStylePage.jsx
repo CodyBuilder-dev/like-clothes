7420c7eb653677b18b65fca664e8bcdc92b78ada
Supergrammer <supergrammer@naver.com>
2020-04-30 18:02:36 +0900

    [Feat] ChoiceStylePage style modified

--- front-end/src/pages/ChoiceStylePage.jsx
+++ front-end/src/pages/ChoiceStylePage.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Card, GridList, GridListTile, Modal, Paper, Container, Button } from '@material-ui/core';
+import { Box, Card, GridList, GridListTile, Modal, Paper, Container, Button } from '@material-ui/core';
 import { searchClothesRandom } from '../module/searchClothesRandom';
 import { withStyles } from '@material-ui/core/styles';
+import { FavoriteRounded } from '@material-ui/icons'
 import { choicestylejsx } from '../css/useStyles';
 import axios from 'axios';
 import '../css/InfiniteScrollContainer.css';
@@ -21,7 +22,7 @@ function getModalStyle() {
     transform: `translate(-${top}%, -${left}%)`,
   };
 }
-const numItemsPerColumn = 3, maxNumOfChoicedImage = 5, maxNumOfDepth = 3;
+const numItemsPerColumn = 4, maxNumOfChoicedImage = 3, maxNumOfDepth = 5;
 let numOfChoicedImage = 0, numOfDepth = 0;
 
 class ChoiceStylePage extends Component {
@@ -41,8 +42,8 @@ class ChoiceStylePage extends Component {
       if (searchDataList.length > 0) {
         let imgList = searchDataList.map((searchData, index) => {
           return (
-            <img key={index} src = { searchData.img } id={searchData.id}
-            className={this.props.classes.image} style={{ width: "100%", height: "100%", }} />
+            <img key={index} src={searchData.img} id={searchData.id}
+              className={this.props.classes.image} style={{ width: "100%", height: "100%", }} />
           )
         });
         resolve(imgList);
@@ -54,17 +55,17 @@ class ChoiceStylePage extends Component {
         ...this.state,
         imgTags: res,
       });
-    }).catch((err) => {alert(err)});
+    }).catch((err) => { alert(err) });
   };
   handleChoicedClothesId = (img_id) => {
     const nextChoicedItemObject = this.state.choicedItemObject;
     if (!!this.state.choicedItemObject[img_id]) {
       numOfChoicedImage--;
       delete nextChoicedItemObject[img_id];
-      
+
       this.setState({ choicedItemObject: nextChoicedItemObject, canPush: numOfChoicedImage >= 1, });
     }
-    else if (numOfChoicedImage < maxNumOfChoicedImage ) {
+    else if (numOfChoicedImage < maxNumOfChoicedImage) {
       numOfChoicedImage++;
       nextChoicedItemObject[img_id] = true;
 
@@ -78,8 +79,8 @@ class ChoiceStylePage extends Component {
     const url = `${process.env.REACT_APP_URL}/clothes/record-user-clothes`;
     const config = {
       headers: {
-      "Content-Type": "application/json",
-      "Authorization": localStorage.token,
+        "Content-Type": "application/json",
+        "Authorization": localStorage.token,
       },
     };
 
@@ -90,12 +91,12 @@ class ChoiceStylePage extends Component {
     }
 
     axios.post(url, params, config)
-    .then(res => {
-      console.log('누른 데이터 전달:', res);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+      .then(res => {
+        console.log('누른 데이터 전달:', res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
 
     if (++numOfDepth == maxNumOfDepth) {
       this.setState({ open: true, });
@@ -112,7 +113,7 @@ class ChoiceStylePage extends Component {
     return numOfChoicedImage;
   }
   handleClose = () => {
-    this.setState({ open: false, } );
+    this.setState({ open: false, });
     this.props.history.replace('/');
   }
   componentDidMount() {
@@ -121,35 +122,46 @@ class ChoiceStylePage extends Component {
 
   render() {
     return (
-      <div style={{margin: "-30px auto 0px"}}>
-        <Paper variant="outlined" className={this.props.classes.titlearea}>
-          <span className={this.props.classes.title}>선호 스타일 선택</span>
-        </Paper>
-        <Paper>
-          <span className={this.props.classes.cardheader}> Choice Your Style!</span>
-          <Card style={{ marginTop: "4px" }}>
+      <div>
+        <Card styles={{ flexGrow: 1, padding: 16, paddingTop: 0 }}>
+          <Box border={2} borderRadius={5} style={{
+            padding: 24, textAlign: 'center',
+            color: 'rgb(128, 128, 128)',
+          }}>
+            <p style={{ fontSize: 35, marginTop: 10, marginLeft: 10 }}>내 스타일 고르기</p>
+            <Box border={2} borderRadius={5} style={{
+              padding: 24, textAlign: 'center',
+              color: 'rgb(128, 128, 128)',
+              marginLeft: 30, marginRight: 30
+            }}>
+              <p>이 페이지는 옷 추천을 받기 전에 사용자의 취향을 알아보기 위한 페이지입니다.</p>
+              <span>보여지는 이미지 중에서</span>
+              <span style={{ color: 'red' }}><FavoriteRounded style={{ width: 20, height: 20 }} />취향저격</span>
+              <span> 에 성공한 옷을 최대 {maxNumOfChoicedImage}개 골라 주세요!!</span>
+              <p></p>
+            </Box>
             <Container className={this.props.classes.imagecontainer}>
-              <GridList style={{height:"500px", alignContent: "space-around", }} cols={numItemsPerColumn}>
+              <GridList style={{ marginTop: 50, marginBottom: 50}} cols={numItemsPerColumn}>
                 {this.state.imgTags.map((img, index) => (
-                  <GridListTile className='container' style={{paddingLeft: "0px", textAlign:"center", }} key={index} cols={img.cols || 1}>
+                  <GridListTile className='container' style={{ paddingLeft: "0px", textAlign: "center", }} key={index} cols={img.cols || 1}>
                     <div style={{ overflow: "hidden", justifyContent: "center", alignItems: "center", }}>
                       {img}
                       <div class={!!this.state.choicedItemObject[img.props.id] ? "choiced" : "overlay"}
-                      onClick={() => this.handleChoicedClothesId(img.props.id)} />
+                        onClick={() => this.handleChoicedClothesId(img.props.id)} />
                     </div>
                   </GridListTile>
                 ))}
               </GridList>
             </Container>
-            <div style={{textAlign: "center", paddingBottom: "3%"}}>
-            <Button
-            variant="contained" color="secondary"
-            disabled={!this.state.canPush} onClick={this.handleChoice}>
-              좋아욧
+            <div style={{ textAlign: "center", paddingBottom: "3%" }}>
+              <Button
+                variant="contained" color="secondary"
+                disabled={!this.state.canPush} onClick={this.handleChoice}>
+                좋아욧
             </Button>
             </div>
-          </Card>
-        </Paper>
+          </Box>
+        </Card>
         <Modal
           open={this.state.open}
           onClose={this.handleClose}
@@ -158,11 +170,11 @@ class ChoiceStylePage extends Component {
           style={{ textAlign: "center", }}
         >
           <div style={getModalStyle()} className={this.props.classes.paper}>
-            <h2 id="simple-modal-title">잘했어욧! 모두 골랐어욧!</h2>              
+            <h2 id="simple-modal-title">선호 스타일 선택이 완료되었습니다.</h2>
             <Button
-            variant="contained" color="secondary"
-            disabled={!this.state.canPush} onClick={() => { this.props.history.replace("/"); }}
-            style={{ align: "center", }}>
+              variant="contained" color="secondary"
+              disabled={!this.state.canPush} onClick={() => { this.props.history.replace("/"); }}
+              style={{ align: "center", }}>
               Main페이지 이동
           </Button>
           </div>
